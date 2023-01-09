c1fd8680bdaf901965846dba86b479e80f2d3af1
Nicola Lerra <github.nicolalerra@gmail.com>
2023-01-09 18:48:16 +0100

    Aggiungere i file di progetto.

---
+++ BE.U2-W2-D1.Gestione_spedizioni/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace BE.U2_W2_D1.Gestione_spedizioni
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ BE.U2-W2-D1.Gestione_spedizioni/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BE.U2_W2_D1.Gestione_spedizioni.MvcApplication" Language="C#" %>
